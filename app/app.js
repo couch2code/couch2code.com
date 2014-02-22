8fd3541af8a18ca062772dab9b2237c8330f0bc2
Tom Wilson <tom@jackhq.com>
2014-02-21 23:32:40 -0500

    first commit to gh-pages

--- app/app.js
+++
@@ -1,7 +0,0 @@
-require('angular/angular');
-require('ui-router/release/angular-ui-router');
-require('firebase/firebase');
-require('angularfire/angularfire');
-
-angular.module('couch2code', ['ui.router','firebase'])
-  .config(require('./states'))
