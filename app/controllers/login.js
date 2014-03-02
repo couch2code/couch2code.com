c2bca313a6923c44dd0ad2c25e706e1fb3bee95b
James Southard <codefu.sensei@gmail.com>
2014-03-02 15:43:54 -0500

    Fixed cancel button, no longer results in failed login attempt

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,4 +1,4 @@
-module.exports = function($scope, $firebaseSimpleLogin) {
+module.exports = function($scope, $firebaseSimpleLogin, $state) {
   $scope.login = function() {
     $scope.loginObj.$login('password', $scope.user)
       .then(function(user) {
@@ -8,5 +8,11 @@ module.exports = function($scope, $firebaseSimpleLogin) {
         alert('Error with Login');
       });
   };
+
+  $scope.cancel = function() {
+    // If paths to the login form come from more than main, then a ui-router
+    //   version with the newer "back" function may be better here
+    $state.go('main');
+  }
 };
 
