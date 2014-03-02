e3330d6d99b2d0856d94d44602b3b448c82ee339
Tom Wilson <tom@jackhq.com>
2014-03-02 09:01:15 -0500

    added submissions and reviews modules

--- app/states.js
+++ app/states.js
@@ -12,36 +12,6 @@ module.exports = function($stateProvider, $urlRouterProvider) {
       url: '/login',
       controller: require('./controllers/login'),
       template: fs.readFileSync(__dirname + '/templates/login.html')
-    })
-    .state('new-submission', {
-      url: '/submissions/new?challenge',
-      controller: require('./controllers/submissions/new'),
-      template: fs.readFileSync(__dirname + '/templates/submissions/form.html')
-    })
-    .state('show-submission', {
-      url: '/submissions/:id',
-      controller: require('./controllers/submissions/show'),
-      template: fs.readFileSync(__dirname + '/templates/submissions/show.html')
-    })
-    .state('edit-submission', {
-      url: '/submissions/:id/edit',
-      controller: require('./controllers/submissions/edit'),
-      template: fs.readFileSync(__dirname + '/templates/submissions/form.html')
-    })
-    .state('new-review', {
-      url: '/reviews/new?submission', 
-      controller: require('./controllers/reviews/new'),
-      template: fs.readFileSync(__dirname + '/templates/reviews/form.html')
-    })
-    .state('show-review', {
-      url: '/reviews/:id',
-      controller: require('./controllers/reviews/show'),
-      template: fs.readFileSync(__dirname + '/templates/reviews/show.html')
-    })
-    .state('edit-review', {
-      url: '/reviews/:id/edit',
-      controllers: require('./controllers/reviews/edit'),
-      template: fs.readFileSync(__dirname + '/templates/reviews/form.html')
     });
 };
 
